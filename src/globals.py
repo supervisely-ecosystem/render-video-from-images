fb7a637f3d09e22f2dbc0c82d45842726ac0736d
cxnt <pavelbartsits@gmail.com>
2022-06-10 17:09:14 +0300

    fix output url path, sly version upgrade, min instance version added

--- src/globals.py
+++ src/globals.py
@@ -1,8 +1,9 @@
 import os
-import supervisely_lib as sly
+import supervisely as sly
+from supervisely.app.v1.app_service import AppService
 
 
-my_app = sly.AppService()
+my_app = AppService()
 api: sly.Api = my_app.public_api
 
 TEAM_ID = int(os.environ['context.teamId'])
